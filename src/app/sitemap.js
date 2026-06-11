431c670b3d62b12c71543918e5715946621c5f2d
Lucas Queiroz <lqvieira7@gmail.com>
2026-06-11 16:46:02 -0300

    fix: next public site url

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,5 +1,5 @@
 export default function sitemap() {
-  const base = process.env.NEXT_PUBLIC_SITE_URL;
+  const base = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://localhost:3000';
   return [
     {
       url: base,
