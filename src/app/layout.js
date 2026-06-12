04346b633c9d2396599096a97fb16d1bca50d82e
Lucas Queiroz <lqvieira7@gmail.com>
2026-06-11 22:21:08 -0300

    feat: vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -106,6 +107,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>{children}</body>
       <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
